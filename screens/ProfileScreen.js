b586408923ce3f7de8836dfc20a0d8b893be82ea
Lembalemba <franklembalemba3@gmail.com>
2024-10-01 14:04:24 +0200

    made made

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -29,7 +29,7 @@ const ProfileScreen = ({ navigation }) => {
 
 
   const handleSignOut = () => {
-    updateAuthentication(false);
+    // updateAuthentication(false);
     navigation.navigate("Signin"); // Adjust according to your navigation
   };
 
